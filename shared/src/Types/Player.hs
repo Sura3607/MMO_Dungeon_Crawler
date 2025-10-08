f0382018efe7683f12750307526064227caa0fba
trongjhuongwr <nguyentronghuong05@gmail.com>
2025-10-08 17:30:58 +0700

    fix server

--- shared/src/Types/Player.hs
+++ shared/src/Types/Player.hs
@@ -1,20 +1,24 @@
-{-# LANGUAGE DeriveGeneric #-} -- << THÊM DÒNG NÀY VÀO ĐẦU TIÊN
+{-# LANGUAGE DeriveGeneric #-} -- Bật tính năng DeriveGeneric
 
 module Types.Player where
 
 import GHC.Generics (Generic)
 import Data.Binary (Binary)
 
+-- Định nghĩa kiểu dữ liệu cho người chơi
 data Player = Player
   { playerId :: Int
   , playerName :: String
-  } deriving (Show, Eq, Generic) -- Bây giờ trình biên dịch sẽ hiểu `Generic`
+  } deriving (Show, Eq, Generic)
 
+-- Khai báo để thư viện Binary biết cách mã hóa/giải mã Player
 instance Binary Player
 
+-- Định nghĩa các lệnh mà người chơi có thể gửi đi
 data PlayerCommand
   = Move { velX :: Float, velY :: Float }
   | Shoot
-  deriving (Show, Eq, Generic) -- Và ở đây nữa
+  deriving (Show, Eq, Generic)
 
+-- Khai báo để thư viện Binary biết cách mã hóa/giải mã PlayerCommand
 instance Binary PlayerCommand
