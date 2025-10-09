6b7bd29c413b20fc506c52ddde5a137b64b67c00
trongjhuongwr <nguyentronghuong05@gmail.com>
2025-10-09 13:01:23 +0700

    Update and Done Phrase 0

--- server/src/Core/Types.hs
+++ server/src/Core/Types.hs
@@ -1,21 +1,21 @@
--- server/src/Core/Types.hs
 module Core.Types where
 
-import Types.Player (PlayerCommand) -- Import từ gói shared
 import Network.Socket (SockAddr)
+-- Import các kiểu đã được chuẩn hóa từ thư viện shared
+import Types.Common (EntityID, Vec2)
+import Types.Player (PlayerCommand)
 
 type Tick = Int
-type EntityID = Int
 
-data Vec2 = Vec2 { vx :: Double, vy :: Double } deriving (Show, Eq)
-
--- Trạng thái của toàn bộ game
+-- | Trạng thái của toàn bộ game phía server.
 data GameState = GameState
-  { gsTick :: Tick
-  , gsCommands :: [(SockAddr, PlayerCommand)] -- Danh sách các lệnh nhận được trong tick này
+  { gsTick      :: Tick
+  , gsCommands  :: [(SockAddr, PlayerCommand)] -- Các lệnh nhận được trong tick
+  -- Sau này sẽ thêm State của người chơi, quái, v.v.
+  -- , gsPlayers :: Map EntityID PlayerState
   }
 
--- Hàm tạo một GameState ban đầu
+-- | Hàm tạo một GameState ban đầu.
 initialGameState :: GameState
 initialGameState = GameState
   { gsTick = 0
