e66788a17569ad9559024e61ad1d6f286afc6cd5
trongjhuongwr <nguyentronghuong05@gmail.com>
2025-10-09 12:39:14 +0700

    Add WorldSnapshot

--- shared/src/Network/Packet.hs
+++ shared/src/Network/Packet.hs
@@ -1,5 +1,18 @@
+-- Tệp này sẽ định nghĩa gói tin quan trọng nhất: WorldSnapshot.
+-- Đây là "bức ảnh" về trạng thái thế giới game mà server gửi cho client mỗi tick.
+{-# LANGUAGE DeriveGeneric #-}
+
 module Network.Packet where
 
--- Shared packet definitions (placeholder)
+import Data.Binary (Binary)
+import GHC.Generics (Generic)
+import Types.Player (PlayerState)
 
-data Packet = DummyPacket deriving (Show)
+-- | Trạng thái của toàn bộ thế giới game tại một thời điểm.
+-- Server sẽ gửi gói tin này về cho tất cả client trong mỗi tick.
+data WorldSnapshot = WorldSnapshot
+  { wsPlayers :: [PlayerState]
+  -- Sau này sẽ thêm: wsEnemies, wsBullets, wsItems, v.v.
+  } deriving (Show, Generic)
+
+instance Binary WorldSnapshot
