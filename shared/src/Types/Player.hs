079e4aa0a38f6e9366c6246d01ceaeb5308ff06e
trongjhuongwr <nguyentronghuong05@gmail.com>
2025-10-09 12:37:32 +0700

    Add PlayState cho Player.hs

--- shared/src/Types/Player.hs
+++ shared/src/Types/Player.hs
@@ -1,24 +1,45 @@
-{-# LANGUAGE DeriveGeneric #-} -- Bật tính năng DeriveGeneric
+-- Tệp này ...
+{-# LANGUAGE DeriveGeneric #-}
 
 module Types.Player where
 
-import GHC.Generics (Generic)
 import Data.Binary (Binary)
+import GHC.Generics (Generic)
+import Types.Common (Angle, EntityID, Vec2)
 
--- Định nghĩa kiểu dữ liệu cho người chơi
+-- | Dữ liệu định danh cho một người chơi (phù hợp cho lobby, database).
 data Player = Player
-  { playerId :: Int
+  { playerId   :: Int
   , playerName :: String
   } deriving (Show, Eq, Generic)
 
--- Khai báo để thư viện Binary biết cách mã hóa/giải mã Player
 instance Binary Player
 
--- Định nghĩa các lệnh mà người chơi có thể gửi đi
+-- | Lệnh mà Client gửi lên Server trong mỗi tick.
 data PlayerCommand
-  = Move { velX :: Float, velY :: Float }
+  -- | Hướng di chuyển mong muốn (ví dụ: Vec2 0 1 là đi thẳng lên).
+  = Move Vec2
+  -- | Góc của nòng súng so với trục hoành.
+  | Aim Angle
+  -- | Lệnh bắn.
   | Shoot
-  deriving (Show, Eq, Generic)
+  -- | Gói tin tối ưu, gửi cả hướng di chuyển và góc bắn cùng lúc.
+  | FullCommand Vec2 Angle
+  deriving (Show, Generic)
 
--- Khai báo để thư viện Binary biết cách mã hóa/giải mã PlayerCommand
 instance Binary PlayerCommand
+
+-- | Trạng thái vật lý của người chơi trong thế giới game.
+-- Server quản lý và gửi về Client trong WorldSnapshot.
+data PlayerState = PlayerState
+  { -- | ID của thực thể xe tăng này.
+    psId          :: EntityID
+    -- | Vị trí hiện tại trên bản đồ.
+  , psPos         :: Vec2
+    -- | Góc xoay của thân xe (thường đi theo hướng di chuyển).
+  , psBodyAngle   :: Angle
+    -- | Góc xoay của nòng súng (đi theo con trỏ chuột).
+  , psTurretAngle :: Angle
+  } deriving (Show, Eq, Generic)
+
+instance Binary PlayerState
