034d0c0e504b06b1a37db5e8b16482ea544a0943
trongjhuongwr <nguyentronghuong05@gmail.com>
2025-10-09 12:40:44 +0700

    fix

--- shared/src/Network/Packet.hs
+++ shared/src/Network/Packet.hs
@@ -10,7 +10,8 @@ import Types.Player (PlayerState)
 
 -- | Trạng thái của toàn bộ thế giới game tại một thời điểm.
 -- Server sẽ gửi gói tin này về cho tất cả client trong mỗi tick.
-data WorldSnapshot = WorldSnapshot
+-- Dùng newtype để tối ưu, vì nó là zero-cost abstraction.
+newtype WorldSnapshot = WorldSnapshot
   { wsPlayers :: [PlayerState]
   -- Sau này sẽ thêm: wsEnemies, wsBullets, wsItems, v.v.
   } deriving (Show, Generic)
