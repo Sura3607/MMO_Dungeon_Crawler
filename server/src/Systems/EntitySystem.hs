9a2e3f6fcc328ca9e19b199102adc9fb4938ebb6
Nhựt Nguyễn <nguyenminhnhut3607@gmail.com>
2025-10-11 20:28:23 +0700

    feat(entitySyt) : logic

--- server/src/Systems/EntitySystem.hs
+++ server/src/Systems/EntitySystem.hs
@@ -1,4 +1,11 @@
-module Systems.EntitySystem where
+module Systems.EntitySystem (updatePlayerState) where
 
-updateEntities :: IO ()
-updateEntities = return ()
+import Core.Types (GameState(..))
+import Types.Player (PlayerState(..))
+
+-- | Hàm này sẽ tìm và cập nhật trạng thái cho một người chơi cụ thể.
+-- Hiện tại, để đơn giản, chúng ta giả định chỉ có một người chơi và cập nhật thẳng.
+-- Sau này sẽ cần playerId để phân biệt.
+updatePlayerState :: PlayerState -> GameState -> GameState
+updatePlayerState newPlayerState gs =
+  gs { gsPlayers = [newPlayerState] } -- Ghi đè state của người chơi đầu tiên
